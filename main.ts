01f582cdb7cf9b3fab3ebc20c10798ddfc8002e0
AnaLopeez <153504381+AnaLopeez@users.noreply.github.com>
2024-04-10 13:06:22 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    maqueen.motorStop(maqueen.Motors.M1)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    maqueen.motorStop(maqueen.Motors.M2)
+    basic.pause(1000)
+})
